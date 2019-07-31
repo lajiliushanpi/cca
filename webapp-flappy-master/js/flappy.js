449efee8bbb2905d281fa4625f6a27f7c52227f4
lajiliushanpi <46955261+lajiliushanpi@users.noreply.github.com>
2019-07-31 23:21:57 +0800

    Add files via upload

--- webapp-flappy-master/js/flappy.js
+++ webapp-flappy-master/js/flappy.js
@@ -9,8 +9,8 @@ function preload() {
   game.load.image("playerImg","../assets/flappy-cropped.png");
   game.load.audio("score", "../assets/point.ogg");
   game.load.image("pipe","../assets/pipe_orange.png");
+  game.load.image("pipeEnd","../assets/pipe-end.png")
 }
-
 function create() {
   game.stage.setBackgroundColor("#00FFFF");
   labelScore = game.add.text(20, 20, "-2", {font: "60px Arial", fill: "#000000"});
@@ -19,15 +19,17 @@ function create() {
   game.physics.arcade.enable(player);
   player.body.gravity.y = 1000;
   game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(playerJump);
-
+  player.anchor.setTo(0.5, 0.5);
   game.time.events.loop(pipeInterval * Phaser.Timer.SECOND, generatePipe);
 }
 
 function update() {
   game.physics.arcade.overlap(player, pipes, gameOver);
-  if (player.y<=0 || player.y>=400){
-    gameOver()
+  player.rotation = Math.atan(player.body.velocity.y / 500);
+  if (player.y<=0-30 || player.y>=400){
+    gameOver();
   }
+
 }
 
 function addPipeBlock(x, y) {
@@ -37,14 +39,31 @@ function addPipeBlock(x, y) {
   block.body.velocity.x = -200;
 }
 
+var gapMargin = 40;
+var height = 400;
+var width = 790;
+var blockHeight = 50;
+var pipeEndExtraWidth = 10;
+var pipeEndHeight = 25;
+var gapSize = 150;
 function generatePipe() {
-  var gapStart = game.rnd.integerInRange(1, 5);
-  for (var count = 0; count < 8; count++) {
-    if(count != gapStart && count != gapStart+1){
-      addPipeBlock(750, count * 50);
+  var gapStart = game.rnd.integerInRange(gapMargin, height - gapSize - gapMargin);
+    addPipeEnd(width - (pipeEndExtraWidth / 2), gapStart);
+    for(var y = gapStart; y > 0; y -= blockHeight) {
+      addPipeBlock(width, y - blockHeight);
     }
+    addPipeEnd(width - (pipeEndExtraWidth / 2), gapStart + gapSize);
+   for(var y = gapStart + gapSize + pipeEndHeight; y < height; y += blockHeight) {
+     addPipeBlock(width, y);
+   }
+    changeScore();
   }
-  changeScore();
+
+function addPipeEnd(x,y){
+  var block = game.add.sprite(x,y,"pipeEnd");
+  pipes.push(block);
+  game.physics.arcade.enable(block);
+  block.body.velocity.x = -200
 }
 
 function playerJump() {
@@ -57,5 +76,6 @@ function changeScore() {
   labelScore.setText(score.toString());
 }
 function gameOver() {
+  alert("You lost,try again!Your score is:"+score);
   location.reload()
 }
